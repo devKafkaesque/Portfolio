1d48490df93a67d5f18868bad6306faef4708ddd
invisGhoul420 <parasharvishist@gmail.com>
2024-12-25 11:50:31 +0530

    added Learn_React Project
    Signed-off-by: invisGhoul420 <parasharvishist@gmail.com>

--- static/js/container-dropdown.js
+++ static/js/container-dropdown.js
@@ -1,6 +1,20 @@
 let dj = document.getElementsByClassName('container-pointer')[0];
-let Athlead = document.getElementById('first-container');
+let react = document.getElementsByClassName('container-pointer')[1];
+let Athlead = document.getElementById('second-container');
+let learn_react = document.getElementById('third-container');
 
+learn_react.addEventListener('click',function () {
+
+});
+
+learn_react.addEventListener('mouseover', function () {
+    react.style.transform = 'scale(2.3)';
+    react.style.borderRadius = '50%';
+});
+
+learn_react.addEventListener('mouseout', function () {
+    react.style.transform = 'scale(1)';
+});
 
 Athlead.addEventListener('mouseover', function () {
     dj.style.transform = 'scale(2.3) rotate(-90deg) translateY(-10%)';
