bb7f97d28e02906000d4daf965aec07c464d6210
invisGhoul420 <parasharvishist@gmail.com>
2024-12-27 03:10:57 +0530

    added Custom Right-Click
    Signed-off-by: invisGhoul420 <parasharvishist@gmail.com>

--- static/js/fan.js
+++ static/js/fan.js
@@ -1,17 +1,25 @@
 let jap_fan = document.getElementById('jap_fan');
 let maxClicks = 5;
 let clicks = 0;
+let timeout;
 
-function addClicks() {
+function timer_exec() {
     clicks++;
-    if (clicks === maxClicks) {
-        console.log('Max clicks reached');
-        clicks = 0;
+
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        clicks = 0
+    }, 1000);
+
+    if (clicks >= maxClicks) {
+        console.log('damn');
+        jap_fan.style.pointerEvents = 'none';
     }
 }
 
 if (jap_fan) {
-    jap_fan.addEventListener('click', addClicks);
+    jap_fan.addEventListener('click', timer_exec);
 } else {
     console.warn('Element with id "jap_fan" not found!');
 }
